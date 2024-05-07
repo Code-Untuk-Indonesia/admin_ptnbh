89ae0d77ada09e90a650c814c9f8774b6e2d407d
Adeveloper <ademnurhadi@gmail.com>
2024-05-08 00:31:17 +0700

    push remon

---
+++ resources/views/berita/berita.blade.php
@@ -0,0 +1,147 @@
+@extends('admin-template.navbar-footer')
+@section('content')
+    <div class="app-wrapper">
+
+        <div class="app-content pt-3 p-md-3 p-lg-4">
+            <div class="container-xl">
+
+                <div class="row g-3 mb-4 align-items-center justify-content-between">
+                    <div class="col-auto">
+                        <h1 class="app-page-title mb-0">Orders</h1>
+                    </div>
+                    <div class="col-auto">
+                        <div class="page-utilities">
+                            <div class="row g-2 justify-content-start justify-content-md-end align-items-center">
+                                <div class="col-auto">
+                                    <form class="table-search-form row gx-1 align-items-center">
+                                        <div class="col-auto">
+                                            <input type="text" id="search-orders" name="searchorders"
+                                                class="form-control search-orders" placeholder="Search">
+                                        </div>
+                                        <div class="col-auto">
+                                            <button type="submit" class="btn app-btn-secondary">Search</button>
+                                        </div>
+                                    </form>
+
+                                </div><!--//col-->
+                                <div class="col-auto">
+
+                                    <select class="form-select w-auto">
+                                        <option selected value="option-1">All</option>
+                                        <option value="option-2">This week</option>
+                                        <option value="option-3">This month</option>
+                                        <option value="option-4">Last 3 months</option>
+
+                                    </select>
+                                </div>
+                                <div class="col-auto">
+                                    <a class="btn app-btn-secondary" href="#">
+                                        <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-download me-1"
+                                            fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                            <path fill-rule="evenodd"
+                                                d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z" />
+                                            <path fill-rule="evenodd"
+                                                d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z" />
+                                        </svg>
+                                        Download CSV
+                                    </a>
+                                </div>
+                            </div><!--//row-->
+                        </div><!--//table-utilities-->
+                    </div><!--//col-auto-->
+                </div><!--//row-->
+
+
+                <a class="btn app-btn-secondary mb-2" href="#">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="1.5em" width="1.5em" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z"/></svg>
+                    Tambah Berita
+                </a>
+
+                <div class="tab-content" id="orders-table-tab-content">
+                    <div class="tab-pane fade show active" id="orders-all" role="tabpanel" aria-labelledby="orders-all-tab">
+                        <div class="app-card app-card-orders-table shadow-sm mb-5">
+                            <div class="app-card-body">
+                                <div class="table-responsive">
+                                    <table class="table app-table-hover mb-0 text-left">
+                                        <thead>
+                                            <tr>
+                                                <th class="cell">No</th>
+                                                <th class="cell">Gambar</th>
+                                                <th class="cell">Judul</th>
+                                                <th class="cell">Konten</th>
+                                                <th class="cell">Date</th>
+                                                <th class="cell"></th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+
+                                            <tr>
+                                                <td class="cell">#1</td>
+                                                <td class="cell">John Sanders</td>
+                                                <td class="cell"><span class="truncate">Lorem t</span></td>
+                                                <td class="cell"><span class="truncate">Lorem, ipsum dolor sit amet
+                                                        consectetur adipisicing elit. Ducimus natus vitae animi et facilis
+                                                        aspernatur amet cum officia eveniet magni molestiae dolor ea, esse
+                                                        quam expedita voluptates id impedit perspiciatis.</span></td>
+
+                                                <td class="cell"><span>17 Oct 2024</span><span class="note">2:16
+                                                        PM</span></td>
+
+                                                <td class="cell"><a class="btn-sm app-btn-danger" href="#">Hapus</a>
+                                                    <a class="btn-sm app-btn-primary" href="#">Edit</a>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td class="cell">#1</td>
+                                                <td class="cell">John Sanders</td>
+                                                <td class="cell"><span class="truncate">Lorem t</span></td>
+                                                <td class="cell"><span class="truncate">Lorem, ipsum dolor sit amet
+                                                        consectetur adipisicing elit. Ducimus natus vitae animi et facilis
+                                                        aspernatur amet cum officia eveniet magni molestiae dolor ea, esse
+                                                        quam expedita voluptates id impedit perspiciatis.</span></td>
+
+                                                <td class="cell"><span>17 Oct 2024</span><span class="note">2:16
+                                                        PM</span></td>
+
+                                                <td class="cell"><a class="btn-sm app-btn-danger" href="#">Hapus</a>
+                                                    <a class="btn-sm app-btn-primary" href="#">Edit</a>
+                                                </td>
+                                            </tr>
+
+
+
+
+                                        </tbody>
+                                    </table>
+                                </div><!--//table-responsive-->
+
+                            </div><!--//app-card-body-->
+                        </div><!--//app-card-->
+                        <nav class="app-pagination">
+                            <ul class="pagination justify-content-center">
+                                <li class="page-item disabled">
+                                    <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Previous</a>
+                                </li>
+                                <li class="page-item active"><a class="page-link" href="#">1</a></li>
+                                <li class="page-item"><a class="page-link" href="#">2</a></li>
+                                <li class="page-item"><a class="page-link" href="#">3</a></li>
+                                <li class="page-item">
+                                    <a class="page-link" href="#">Next</a>
+                                </li>
+                            </ul>
+                        </nav><!--//app-pagination-->
+
+                    </div><!--//tab-pane-->
+
+
+                </div><!--//tab-content-->
+
+
+
+            </div><!--//container-fluid-->
+        </div><!--//app-content-->
+
+
+
+    </div><!--//app-wrapper-->
+@endsection
---
+++ resources/views/berita/create-berita.blade.php
@@ -0,0 +1,95 @@
+@extends('admin-template.navbar-footer')
+@section('content')
+    <div class="app-wrapper">
+
+        <div class="app-content pt-3 p-md-3 p-lg-4">
+            <div class="container-xl">
+
+                <div class="row g-3 mb-4 align-items-center justify-content-between">
+                    <div class="col-auto">
+                        <h1 class="app-page-title mb-0">Tambah Berita</h1>
+                    </div>
+
+                </div><!--//row-->
+
+
+
+
+            </div><!--//container-fluid-->
+        </div><!--//app-content-->
+
+        <div class="container">
+            <div class="col-12 col-md-12">
+                <div class="app-card app-card-settings shadow-sm p-4">
+
+                    <div class="app-card-body">
+                        <form class="settings-form">
+
+                            <div class="mb-3">
+                                <label for="upload-gambar" class="form-label">Upload Gambar<span class="ms-2"
+                                        data-bs-container="body" data-bs-toggle="popover" data-bs-trigger="hover focus"
+                                        data-bs-placement="top"
+                                        data-bs-content="Ini adalah contoh popover Bootstrap. Anda dapat menggunakan popover untuk memberikan informasi tambahan."><svg
+                                            width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-info-circle" fill="currentColor"
+                                            xmlns="http://www.w3.org/2000/svg">
+                                            <path fill-rule="evenodd"
+                                                d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                                            <path
+                                                d="M8.93 6.588l-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588z" />
+                                            <circle cx="8" cy="4.5" r="1" />
+                                        </svg></span></label>
+                                <input type="file" class="form-control" id="upload-gambar" name="upload-gambar">
+                            </div>
+
+                            <div class="mb-3">
+                                <label for="setting-input-1" class="form-label">Judul<span class="ms-2"
+                                        data-bs-container="body" data-bs-toggle="popover" data-bs-trigger="hover focus"
+                                        data-bs-placement="top"
+                                        data-bs-content="This is a Bootstrap popover example. You can use popover to provide extra info."><svg
+                                            width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-info-circle"
+                                            fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                            <path fill-rule="evenodd"
+                                                d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                                            <path
+                                                d="M8.93 6.588l-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588z" />
+                                            <circle cx="8" cy="4.5" r="1" />
+                                        </svg></span></label>
+                                <input type="text" class="form-control" id="setting-input-1" value="Lorem Ipsum Ltd."
+                                    required>
+                            </div>
+
+                            <div class="mb-3">
+                                <label for="setting-input-3" class="form-label">Konten</label>
+                                <textarea id="summernote"></textarea>
+                            </div>
+                            <button type="submit" class="btn app-btn-primary">Buat Berita</button>
+                        </form>
+                    </div><!--//app-card-body-->
+
+                </div><!--//app-card-->
+            </div>
+
+        </div>
+
+
+
+    </div><!--//app-wrapper-->
+
+    <script>
+        $(document).ready(function() {
+            $('#summernote').summernote({
+                height: 300,
+                toolbar: [
+                    ['style', ['style']],
+                    ['font', ['bold', 'italic', 'underline', 'clear']],
+                    ['fontsize', ['fontsize']],
+                    ['color', ['color']],
+                    ['para', ['ul', 'ol', 'paragraph']],
+                    ['table', ['table']],
+                    ['insert', ['link', 'picture', 'video']],
+                    ['view', ['fullscreen', 'codeview']],
+                ]
+            });
+        });
+    </script>
+@endsection
