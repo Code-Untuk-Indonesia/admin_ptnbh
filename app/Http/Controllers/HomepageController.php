1cc84c1c96f5c0ac2a98483459492ced00ce9928
Adeveloper <ademnurhadi@gmail.com>
2024-05-17 21:18:39 +0700

    add:home crud

---
+++ app/Http/Controllers/HomepageController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\home;
+use Illuminate\Http\Request;
+
+class HomepageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $data = home::all();
+        return view('crud-home.ptnbh-section', compact('data'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $data = Home::findOrFail($id);
+        return view('crud-home.edit', compact('data'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $request->validate([
+            'judul_ptnbh' => 'required|string|max:255',
+            'tentang_ptnbh' => 'required|string',
+            'sambutan_rektor' => 'required|string',
+            'gambar_rektor' => 'required|string|max:255',
+        ]);
+
+        $data = Home::findOrFail($id);
+        $data->update([
+            'judul_ptnbh' => $request->judul_ptnbh,
+            'tentang_ptnbh' => $request->tentang_ptnbh,
+            'sambutan_rektor' => $request->sambutan_rektor,
+            'gambar_rektor' => $request->gambar_rektor,
+        ]);
+
+        return redirect()->route('home.index')->with('success', 'Data updated successfully');
+    }
+
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+
+    public function apihome() {
+        $data = home::all();
+        return response()->json($data);
+    }
+}
---
+++ resources/views/crud-home/ptnbh-section.blade.php
@@ -0,0 +1,160 @@
+@extends('admin-template.navbar-footer')
+@section('content')
+    <div class="app-wrapper">
+
+        <div class="app-content pt-3 p-md-3 p-lg-4">
+            <div class="container-xl">
+
+                <div class="row g-3 mb-4 align-items-center justify-content-between">
+                    <div class="col-auto">
+                        <h1 class="app-page-title mb-0">Home</h1>
+                    </div>
+                    <div class="col-auto">
+                        <div class="page-utilities">
+                            <div class="row g-2 justify-content-start justify-content-md-end align-items-center">
+                                <div class="col-auto">
+                                    <form class="table-search-form row gx-1 align-items-center">
+                                        <div class="col-auto">
+                                            <input type="text" id="search-orders" name="searchorders"
+                                                class="form-control search-orders" placeholder="Search">
+                                        </div>
+                                        <div class="col-auto">
+                                            <button type="submit" class="btn app-btn-secondary">Search</button>
+                                        </div>
+                                    </form>
+
+                                </div><!--//col-->
+                                <div class="col-auto">
+
+                                    <select class="form-select w-auto">
+                                        <option selected value="option-1">All</option>
+                                        <option value="option-2">This week</option>
+                                        <option value="option-3">This month</option>
+                                        <option value="option-4">Last 3 months</option>
+
+                                    </select>
+                                </div>
+                                <div class="col-auto">
+                                    <a class="btn app-btn-secondary" href="#">
+                                        <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-download me-1"
+                                            fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                            <path fill-rule="evenodd"
+                                                d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z" />
+                                            <path fill-rule="evenodd"
+                                                d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z" />
+                                        </svg>
+                                        Download CSV
+                                    </a>
+                                </div>
+                            </div><!--//row-->
+                        </div><!--//table-utilities-->
+                    </div><!--//col-auto-->
+                </div><!--//row-->
+
+
+                <a class="btn app-btn-secondary mb-2" href="/create-berita">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="1.5em" width="1.5em"
+                        viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                        <path
+                            d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z" />
+                    </svg>
+                    Tambah Berita
+                </a>
+
+                <div class="tab-content" id="orders-table-tab-content">
+                    <div class="tab-pane fade show active" id="orders-all" role="tabpanel" aria-labelledby="orders-all-tab">
+                        <div class="app-card app-card-orders-table shadow-sm mb-5">
+                            <div class="app-card-body">
+                                <div class="table-responsive">
+                                    <table class="table app-table-hover mb-0 text-left">
+                                        <thead>
+                                            <tr>
+                                                <th class="cell" >Judul </th>
+                                                <th class="cell" >Content PTNBH</th>
+                                                <th class="cell" >aksi</th>
+
+
+                                            </tr>
+                                        </thead>
+
+                                        <tbody>
+                                            @foreach ($data as $item)
+                                            <tr>
+                                                <td class="cell">
+                                                    {{ $item->judul_ptnbh }}
+                                                </td>
+                                                <td class="cell">
+                                                    {{ $item->tentang_ptnbh }}
+                                                </td>
+                                                <td class="cell">
+                                                    <button class="btn-sm app-btn-secondary">
+                                                        <a href="{{ route('home.edit', $item->id) }}">Edit</a>
+
+                                                    </button>
+                                                </td>
+                                            </tr>
+                                            @endforeach
+
+                                        </tbody>
+                                    </table>
+
+                                </div><!--//table-responsive-->
+
+                            </div><!--//app-card-body-->
+                        </div><!--//app-card-->
+                        <div class="table-responsive">
+                            <table class="table app-table-hover mb-0 text-left">
+                                <thead>
+                                    <tr>
+                                        <th class="cell" >Foto Rektor </th>
+                                        <th class="cell" >Sambutan Rektor</th>
+                                        <th></th>
+
+                                    </tr>
+                                </thead>
+
+                                <tbody>
+                                    <tr>
+                                        <td class="cell" >
+                                           <img src="{{asset('template-admin/assets/images/profiles/profile-1.png')}}" alt="">
+                                        </td>
+                                        <td class="cell">
+                                            PTNBH adalah perusahaan yang bergerak di bidang penyediaan jasa
+                                            pendidikan dan pengembangan sumber daya manusia.
+                                        </td>
+                                    </tr>
+
+                                </tbody>
+                            </table>
+
+                        </div><!--//table-responsive-->
+                        {{-- <nav class="app-pagination">
+                            <ul class="pagination justify-content-center">
+                                <li class="page-item disabled">
+                                    <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Previous</a>
+                                </li>
+                                <li class="page-item active"><a class="page-link" href="#">1</a></li>
+                                <li class="page-item"><a class="page-link" href="#">2</a></li>
+                                <li class="page-item"><a class="page-link" href="#">3</a></li>
+                                <li class="page-item">
+                                    <a class="page-link" href="#">Next</a>
+                                </li>
+                            </ul>
+                        </nav><!--//app-pagination--> --}}
+
+                    </div><!--//tab-pane-->
+
+
+                </div><!--//tab-content-->
+
+
+
+            </div><!--//container-fluid-->
+        </div><!--//app-content-->
+
+
+
+    </div><!--//app-wrapper-->
+
+
+@endsection
