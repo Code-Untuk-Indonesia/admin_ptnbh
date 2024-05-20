7e87b8f1ec6e5c64c08f5d5426e90f80398629b1
Adeveloper <ademnurhadi@gmail.com>
2024-05-20 16:13:17 +0700

    en

--- resources/views/crud-tentang/edit.blade.php
+++ resources/views/crud-tentang/edit.blade.php
@@ -10,28 +10,28 @@
                 @method('PUT')
 
                 <div class="form-group">
-                    <label for="judul_sejarah">Judul Sejarah</label>
-                    <input type="text" name="judul_sejarah" class="form-control" value="{{ old('judul_sejarah', $data->judul_sejarah) }}" required>
+                    <label for="judul_sejarah_id">Judul Sejarah</label>
+                    <input type="text" name="judul_sejarah_id" class="form-control" value="{{ old('judul_sejarah_id', $data->judul_sejarah_id) }}" required>
                 </div>
 
                 <div class="form-group">
-                    <label for="title_history">Judul Sejarah (Eng)</label>
-                    <input type="text" name="title_history" class="form-control" value="{{ old('title_history', $data->title_history) }}" required>
+                    <label for="judul_sejarah_en">Judul Sejarah (Eng)</label>
+                    <input type="text" name="judul_sejarah_en" class="form-control" value="{{ old('judul_sejarah_en', $data->judul_sejarah_en) }}" required>
                 </div>
 
                 <div class="form-group">
-                    <label for="isi_sejarah">Isi Sejarah</label>
-                    <textarea name="isi_sejarah" id="summernote" class="form-control" required>{{ old('isi_sejarah', $data->isi_sejarah) }}</textarea>
+                    <label for="isi_sejarah_id">Isi Sejarah</label>
+                    <textarea name="isi_sejarah_id" id="summernote" class="form-control" required>{{ old('isi_sejarah_id', $data->isi_sejarah_id) }}</textarea>
                 </div>
 
                 <div class="form-group">
-                    <label for="content_history">Isi Sejarah (Eng)</label>
-                    <textarea name="content_history" id="summernote2"  class="form-control" required>{{ old('content_history', $data->content_history) }}</textarea>
+                    <label for="isi_sejarah_en">Isi Sejarah (Eng)</label>
+                    <textarea name="isi_sejarah_en" id="summernote2"  class="form-control" required>{{ old('isi_sejarah_en', $data->isi_sejarah_en) }}</textarea>
                 </div>
 
                 <div class="form-group">
-                    <label for="visi">Visi</label>
-                    <textarea name="visi" class="form-control"  id="summernote3"  required>{{ old('visi', $data->visi) }}</textarea>
+                    <label for="visi_id">Visi</label>
+                    <textarea name="visi_id" class="form-control"  id="summernote3"  required>{{ old('visi_id', $data->visi_id) }}</textarea>
                 </div>
 
                 <div class="form-group">
@@ -40,8 +40,8 @@
                 </div>
 
                 <div class="form-group">
-                    <label for="misi">Misi</label>
-                    <textarea name="misi" class="form-control" id="summernote5" required>{{ old('misi', $data->misi) }}</textarea>
+                    <label for="misi_id">Misi</label>
+                    <textarea name="misi_id" class="form-control" id="summernote5" required>{{ old('misi_id', $data->misi_id) }}</textarea>
                 </div>
 
                 <div class="form-group">
